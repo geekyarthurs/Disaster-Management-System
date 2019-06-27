3ba3bee97d7d67c3fba67cd544f85fe06a1701de
Mahesh C. Regmi <36955694+geekyarthurs@users.noreply.github.com>
2019-06-27 10:54:56 +0545

    NodeMCU Code

---
+++ nodeMcu.c
@@ -0,0 +1,194 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <Wire.h>
+const char* ssid = "DESKTOP-72RVD8R 1824";
+const char* password = "3t3302P,";
+
+const uint8_t MPU6050SlaveAddress = 0x68;
+
+// Select SDA and SCL pins for I2C communication 
+const uint8_t scl = D6;
+const uint8_t sda = D7;
+
+// sensitivity scale factor respective to full scale setting provided in datasheet 
+const uint16_t AccelScaleFactor = 16384;
+const uint16_t GyroScaleFactor = 131;
+
+// MPU6050 few configuration register addresses
+const uint8_t MPU6050_REGISTER_SMPLRT_DIV   =  0x19;
+const uint8_t MPU6050_REGISTER_USER_CTRL    =  0x6A;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_1   =  0x6B;
+const uint8_t MPU6050_REGISTER_PWR_MGMT_2   =  0x6C;
+const uint8_t MPU6050_REGISTER_CONFIG       =  0x1A;
+const uint8_t MPU6050_REGISTER_GYRO_CONFIG  =  0x1B;
+const uint8_t MPU6050_REGISTER_ACCEL_CONFIG =  0x1C;
+const uint8_t MPU6050_REGISTER_FIFO_EN      =  0x23;
+const uint8_t MPU6050_REGISTER_INT_ENABLE   =  0x38;
+const uint8_t MPU6050_REGISTER_ACCEL_XOUT_H =  0x3B;
+const uint8_t MPU6050_REGISTER_SIGNAL_PATH_RESET  = 0x68;
+
+int16_t AccelX, AccelY, AccelZ, Temperature, GyroX, GyroY, GyroZ;
+
+void setup () {
+ 
+Serial.begin(9600);
+ Wire.begin(sda, scl);
+  MPU6050_Init();
+WiFi.begin(ssid, password);
+ 
+while (WiFi.status() != WL_CONNECTED) {
+ 
+delay(1000);
+Serial.print("Connecting..");
+ 
+}
+ 
+}
+ 
+void loop() {
+
+
+  int currentState = 0; //1 landslide 2 flood 3 vibration
+  
+  double Ax, Ay, Az, T, Gx, Gy, Gz;
+ 
+if (WiFi.status() == WL_CONNECTED) { 
+
+
+  Read_RawValue(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_XOUT_H);
+
+  pinMode(D0,INPUT);
+  pinMode(D1,INPUT);
+
+  int floodVal = digitalRead(D0);
+  int landslideVal = digitalRead(D1);
+
+
+  Gx = (double)GyroX/GyroScaleFactor;
+
+  
+ 
+
+
+  
+
+
+
+  if((Gx >= 2.5 || Gx <= -2.5)){
+    
+    Serial.println("Earthquake");
+       HTTPClient http;  //Declare an object of class HTTPClient
+    
+
+http.begin("http://192.168.137.163:9000/vibrate");
+int httpCode = http.GET();                                                                  //Send the request
+ 
+if (httpCode > 0) { //Check the returning code
+ 
+String payload = http.getString();   //Get the request response payload
+Serial.println(payload);                     //Print the response payload
+ 
+}
+http.end(); 
+    }
+
+   
+
+
+  
+  pinMode(D0,OUTPUT);
+  pinMode(D1,OUTPUT);
+  delay(200);
+  digitalWrite(D0,LOW);
+  digitalWrite(D1,LOW);
+
+
+
+delay(200);
+  
+
+  if(floodVal == HIGH){
+    
+    HTTPClient http;  //Declare an object of class HTTPClient
+    Serial.println("Flooding");
+
+http.begin("http://192.168.137.163:9000/off?problem=Flood");
+int httpCode = http.GET();                                                                  //Send the request
+ 
+if (httpCode > 0) { //Check the returning code
+ 
+String payload = http.getString();   //Get the request response payload
+Serial.println(payload);                     //Print the response payload
+ 
+}
+http.end(); 
+    }
+
+     if(landslideVal == HIGH){
+    
+    HTTPClient http;  //Declare an object of class HTTPClient
+    Serial.println("Landsliding");
+
+http.begin("http://192.168.137.163:9000/off?problem=Landslide");
+int httpCode = http.GET();                                                                  //Send the request
+ 
+if (httpCode > 0) { //Check the returning code
+ 
+String payload = http.getString();   //Get the request response payload
+Serial.println(payload);                     //Print the response payload
+ 
+}
+http.end(); 
+    }
+
+    
+  
+ 
+
+
+ 
+  //Close connection
+ 
+}
+ 
+   //Send a request every 30 seconds
+ 
+}
+
+
+void I2C_Write(uint8_t deviceAddress, uint8_t regAddress, uint8_t data){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
+// read all 14 register
+void Read_RawValue(uint8_t deviceAddress, uint8_t regAddress){
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(regAddress);
+  Wire.endTransmission();
+  Wire.requestFrom(deviceAddress, (uint8_t)14);
+  AccelX = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelY = (((int16_t)Wire.read()<<8) | Wire.read());
+  AccelZ = (((int16_t)Wire.read()<<8) | Wire.read());
+  Temperature = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroX = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroY = (((int16_t)Wire.read()<<8) | Wire.read());
+  GyroZ = (((int16_t)Wire.read()<<8) | Wire.read());
+}
+
+//configure MPU6050
+void MPU6050_Init(){
+  delay(150);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SMPLRT_DIV, 0x07);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_1, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_PWR_MGMT_2, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_CONFIG, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_GYRO_CONFIG, 0x00);//set +/-250 degree/second full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_ACCEL_CONFIG, 0x00);// set +/- 2g full scale
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_FIFO_EN, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_INT_ENABLE, 0x01);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_SIGNAL_PATH_RESET, 0x00);
+  I2C_Write(MPU6050SlaveAddress, MPU6050_REGISTER_USER_CTRL, 0x00);
+}
